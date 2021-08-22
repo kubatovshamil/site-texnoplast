4fc0613551d1c5b0e2d4cb627a3532399b5553c7
kubatovshamil <shamil.kubatov@mail.ru>
2021-08-22 18:42:04 +0300

    Добавил комментарий

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,6 +9,7 @@ use app\models\Category;
 class MainController extends AppController {
 
     public function indexAction(){
+        //Внес вот этот комментарий
         $product = Product::getAllById();
 
         $shares = Product::getShareById();
